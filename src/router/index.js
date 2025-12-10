dd3b6da801d55cfaa2886b233e925cfc34ecf704
Rusiann <Rusiann7@protonmail.com>
2025-12-10 14:15:11 +0800

    cleaned the index

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router' //vue router import
-import { isAuthenticated } from '../utils/auth' //auth import
+import { createRouter, createWebHistory } from "vue-router"; //vue router import
+import { isAuthenticated } from "../utils/auth"; //auth import
 
-<<<<<<< HEAD
-const Student = () =>import('../components/User/Student.vue') //loads components 
-const Teacher = () => import('../components/User/Teacher.vue')
-const Principal = () => import('../components/Admin/Principal.vue')
-const newDashboard = () => import('../components/newDashboard.vue')
-const StEval = () => import('../components/evalforms/student.vue')
-const TcEval = () => import('../components/evalforms/teacher.vue')
-const printable = () => import('../components/evalforms/eval-form-student.vue')
-const printable1 = () => import('../components/evalforms/eval-form-teacher.vue')
-const testSidebar = () => import('../components/Testing/testSidebar.vue')
-const pageError = () => import('../components/notFound.vue')
-const yes = () => import('../components/file.vue')
-const scheduler = () => import('../components/Admin/scheduler.vue')
-=======
 const Student = () => import("../components/User/Student.vue"); //loads components
 const Teacher = () => import("../components/User/Teacher.vue");
 const Principal = () => import("../components/Admin/Principal.vue");
@@ -35,35 +21,21 @@ const test = () => import("../components/Testing/test.vue");
 const graph = () => import("../components/Admin/perf-graph.vue");
 const modal = () => import("../components/Testing/modal.vue");
 const changeQuestiont = () => import("../components/Admin/ChangeQuestiont.vue");
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 5380a09 (finished the question changer and started the split modal type)
-=======
-=======
->>>>>>> Development
 const printablemerge = () =>
   import("../components/evalforms/eval-form-student-merge.vue");
 const printablemerge1 = () =>
   import("../components/evalforms/eval-form-teacher-merge.vue");
-<<<<<<< HEAD
->>>>>>> cea0093 (added the chartjs and implemented the chartjs in the file also added some changes to the api calls url)
-=======
 const graphT = () => import("../components/Admin/perf-grapht.vue");
->>>>>>> Development
 
 const routes = [
   {
-    path: '/', //url
-    name: 'Dashboard', //component
+    path: "/", //url
+    name: "Dashboard", //component
     component: newDashboard, //component to be loaded
-    meta: { title: 'Dashboard', requiresAuth: false } //meta data
+    meta: { title: "Dashboard", requiresAuth: false }, //meta data
   },
 
   {
-<<<<<<< HEAD
-    path: '/student',
-    name: 'Student',
-=======
     path: "/register/student",
     name: "RegisterStudent",
     component: RegisterStudent,
@@ -73,85 +45,80 @@ const routes = [
   {
     path: "/student",
     name: "Student",
->>>>>>> Development
     component: Student,
-    meta: {title: 'Student Form', requiresAuth: true }
+    meta: { title: "Student Form", requiresAuth: true },
   },
 
   {
-    path: '/teacher',
-    name: 'Teacher',
+    path: "/teacher",
+    name: "Teacher",
     component: Teacher,
-    meta: {title: 'Teacher Form', requiresAuth: true }
+    meta: { title: "Teacher Form", requiresAuth: true },
   },
 
   {
-    path: '/principal',
-    name: 'Principal',
+    path: "/principal",
+    name: "Principal",
     component: Principal,
-    meta: { title: 'Principal Dashboard', requiresAuth: false }
+    meta: { title: "Principal Dashboard", requiresAuth: true },
   },
 
   {
-    path: '/student-eval/:id',
-    name: 'student-eval',
+    path: "/student-eval/:id",
+    name: "student-eval",
     component: StEval,
-    meta: {title: 'Student Evaluation', requiresAuth: true}
+    meta: { title: "Student Evaluation", requiresAuth: true },
   },
 
   {
-    path: '/teacher-eval/:id',
-    name: 'teacher-eval',
+    path: "/teacher-eval/:id",
+    name: "teacher-eval",
     component: TcEval,
-    meta: {title: 'Teacher Evalutaion', requiresAuth: true}
+    meta: { title: "Teacher Evalutaion", requiresAuth: true },
   },
 
   {
-    path: '/printable-form-student/:id/:tcrid/:evtid',
-    name: 'printable-form',
+    path: "/printable-form-student/:id/:tcrid/:evtid",
+    name: "printable-form",
     component: printable,
-    meta: {title: 'Printable Form Students', requiresAuth: false}
+    meta: { title: "Printable Form Students", requiresAuth: true },
   },
 
   {
-    path: '/printable-form-teacher/:id/:tcrid/:evtid',
-    name: 'printable-form1',
+    path: "/printable-form-teacher/:id/:tcrid/:evtid",
+    name: "printable-form1",
     component: printable1,
-    meta: {title: 'Printable Form Teachers', requiresAuth: false}
+    meta: { title: "Printable Form Teachers", requiresAuth: true },
   },
 
   {
-    path: '/testSidebar',
-    name: 'testSidebar',
+    path: "/testSidebar",
+    name: "testSidebar",
     component: testSidebar,
-    meta: {title: 'Testing', requiresAuth: false}
+    meta: { title: "Testing", requiresAuth: false },
   },
 
   {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
     component: pageError,
-    meta: {title: '404 Page not found', requiresAuth: false }
+    meta: { title: "404 Page not found", requiresAuth: false },
   },
 
   {
-    path: '/yessssssssssssssssssssssssssssssssssssss',
-    name: 'yes',
+    path: "/yessssssssssssssssssssssssssssssssssssss",
+    name: "yes",
     component: yes,
-    meta: {title: 'Hello', requiresAuth: false }
+    meta: { title: "Hello", requiresAuth: true },
   },
 
   {
-    path: '/scheduler',
-    name: 'Scheduler',
+    path: "/scheduler",
+    name: "Scheduler",
     component: scheduler,
-    meta: {title: 'Scheduler', requiresAuth: false}
-  }
-]
+    meta: { title: "Scheduler", requiresAuth: false },
+  },
 
-<<<<<<< HEAD
-const router = createRouter({ //create the routes and history
-=======
   {
     path: "/changequestions-student",
     name: "Change-Questions",
@@ -218,25 +185,26 @@ const router = createRouter({ //create the routes and history
 
 const router = createRouter({
   //create the routes and history
->>>>>>> 5380a09 (finished the question changer and started the split modal type)
   history: createWebHistory(),
   routes,
-})
+});
 
-// router.beforeEach((to, from, next) => { //jwt token auth guard and verification
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//     if (!isAuthenticated()) {
-//       next({ path: '/login', query: { redirect: to.fullPath } })
-//     } else {
-//       next()
-//     }
-//   } else {
-//     next()
-//   }
+router.beforeEach((to, from, next) => {
+  //jwt token auth guard and verification
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isAuthenticated()) {
+      next({ path: "/login", query: { redirect: to.fullPath } });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 
-//   if (to.meta?.title) { //display the title of the page
-//     document.title = to.meta.title
-//   }
-// })
+  if (to.meta?.title) {
+    //display the title of the page
+    document.title = to.meta.title;
+  }
+});
 
-export default router //export the router as a varaible
+export default router; //export the router as a varaible
